5f130abf9d84a668d378e523890450b36429c909
lokeshkatre <102735711+lokeshkatre@users.noreply.github.com>
2022-10-08 12:23:33 +0530

    Update question3.cpp

--- question3.cpp
+++ question3.cpp
@@ -4,7 +4,7 @@ int peakIndex(int arr[],int n)
 {
     int s=0,ans;
     int e=n-1;
-    int mid= s + (e-s)/2;
+    int mid= (e+s)/2;
     while(s<e)
     {
             if(arr[mid] < arr[mid+1])
@@ -18,7 +18,7 @@ int peakIndex(int arr[],int n)
             else
             e=mid;
             
-            mid= s + (e-s)/2;
+            mid=(e+s)/2;
     }
     return s;
 }
